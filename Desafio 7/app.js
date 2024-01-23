fb21882db9532f381ecf5ec8a41e451be8c8b4be
cacciavillan <102321630+cacciavillan@users.noreply.github.com>
2024-01-23 22:57:51 +0100

    Desafio 7

--- Desafio 7/app.js
+++ Desafio 7/app.js
@@ -61,8 +61,59 @@ function factorial() {
     let numeroUsuario = +prompt("Ingrese un numero")
     let factorial = calculadoraFactorial(numeroUsuario)
 
-    alert(`El número factorial es ${factorial}`)
+    alert(`El número factorial es ${factorial.toFixed(2)}`)
 }
 
-calculadora()
-factorial()
+function conversorReales() {
+    let reales = 4.80;
+    let numero = +prompt("¿Cuantos dólares desea cambiar?");
+    cambio = (numero * reales);
+    return(alert(`Usted recibirá R$${cambio.toFixed(2)}`))
+}
+
+function calculadoraAreaRectangular() { // Area y perímetro
+    let longitud = +prompt("Ingrese el largo de la habitacion en cm")
+    let anchura = +prompt("Ingrese el ancho de la habitación en cm")
+
+    let area = ((longitud / 100) * (anchura / 100));
+    let perimetro = (2 * ((longitud / 100) + (anchura / 100)));
+
+    alert(`El área es ${area.toFixed(2)}m2`);
+    alert(`El perímetro es de ${perimetro.toFixed(2)}m`);
+    
+}
+
+function calculadoraAreaCircular() {
+    let radio = +prompt("Cual es el radio de la habitacion (En centrimetros)")
+
+    let area = (Math.PI * (radio / 100)**2)
+    let perimetro = (2 * Math.PI * (radio / 100))
+
+    alert(`El area es de ${area.toFixed(2)}m2`)
+    alert(`El perimetro es de ${perimetro.toFixed(2)}m`)
+}
+
+function generarTablaMultiplicar(numero) {
+    // Inicializa un array vacio para almacenar los objetos de la tabla.
+    let tabla = [];
+
+    // Utiliza un bucle para generar los elementos de la tabla
+    for (let i = 1; i <= 10; i++) {
+        // Calcular el resultado de la multiplicacion
+        let resultado = numero * i;
+        // Crear un objeto con las pripiedades "Multiplicar" y "Resultado"
+        // Y agregarlo al array
+        tabla.push({multiplicador: i, resultado: resultado});
+    }
+    // Devolver el array que representa la tabla de multiplicar
+    return console.log(tabla);
+}
+
+
+//calculadora();
+//factorial();
+//conversorReales();
+//calculadoraAreaRectangular();
+//calculadoraAreaCircular();
+let valor = +prompt("Ingrese un número del 1 al 10");
+generarTablaMultiplicar(valor)
